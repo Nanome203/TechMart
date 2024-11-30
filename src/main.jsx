f5e7d8dd0151cba14ffbd3440a88fdc78720662e
Hungpq13 <146099528+Hungpq13@users.noreply.github.com>
2024-11-30 15:33:56 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,10 @@ import ProductDetails from "./routes/ProductDetailsPage.jsx";
 import ProfilePage from "./routes/Profile/index.jsx";
 import ErrorPage from "./routes/ErrorPage.jsx";
 import Payment from "./routes/Payment.jsx";
-<<<<<<< HEAD
-import SearchPage from "./routes/SearchPage.jsx"; 
-import Cart from "./routes/Cart.jsx"; 
-=======
 import SearchPage from "./routes/SearchPage.jsx";
->>>>>>> main
 import HomePage from "./routes/HomePage.jsx";
 import TestPage from "./routes/TestPage.jsx";
+import Cart from "./routes/Cart.jsx";
 
 const routerConfig = createBrowserRouter([
   {
@@ -26,12 +22,8 @@ const routerConfig = createBrowserRouter([
         element: <HomePage />,
       },
       {
-<<<<<<< HEAD
-        path: "/products",
-=======
         path: "/products/:id",
         // loader: detailsLoader,
->>>>>>> main
         element: <ProductDetails />,
       },
       {
@@ -47,17 +39,12 @@ const routerConfig = createBrowserRouter([
         element: <SearchPage />,
       },
       {
-<<<<<<< HEAD
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/test",
-        element: <TestPage/>,
-=======
         path: "/test",
         element: <TestPage />,
->>>>>>> main
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
       },
     ],
     errorElement: <ErrorPage />,
